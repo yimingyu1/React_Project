c644caf934b43d910fdea837ba6a319068a0e5b2
yimingyu1 <371044760@qq.com>
2021-09-21 13:48:20 +0800

    login component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,11 +4,40 @@ import logo from './images/logo.png'
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-export default class Login extends Component {
+/**
+ * 登录的路由组件
+ */
 
+export default class Login extends Component {
     onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
+
+    aa = ()=>{
+        console.log(this.formNode);
+        let p = new Promise((resolve, reject)=>{
+            setTimeout(() => {
+                console.log("执行完成promise");
+                resolve("resolve")
+            }, 2000);
+        
+        })
+        console.log(2);
+    }
+
+    validatePwd = (_, value)=>{
+        const regPwd = /^(\w)+$/
+        if (!regPwd.test(value)){
+            return Promise.reject(new Error('密码只能由字母、数字、下划线组成'))
+        }
+        if (value.length < 4){
+            return Promise.reject(new Error('密码长度必须大于4位'))
+        }
+        if (value.length > 12){
+            return Promise.reject(new Error('密码长度必须小于12位'))
+        }
+        return Promise.resolve()
+    }
 
 
     render() {
@@ -25,16 +54,27 @@ export default class Login extends Component {
                         className="login-form"
                         initialValues={{ remember: true }}
                         onFinish={this.onFinish}
+                        ref = {c => this.formNode = c}
                     >
                         <Form.Item
                             name="username"
-                            rules={[{ required: true, message: 'Please input your Username!' }]}
+                            validateFirst="true"
+                            rules={
+                                [
+                                { required: true, whitespace: true,  message: '请输入用户名' },
+                                { pattern: /^(\w)+$/, message: '用户名只能由字母、数字、下划线组成' },
+                                {min: 5, message: '用户名长度必须大于4位'},
+                                {max: 12, message: '用户名不能超过12位'}
+                            ]}
                         >
                             <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名"/>
                         </Form.Item>
                         <Form.Item
                             name="password"
-                            rules={[{ required: true, message: 'Please input your Password!' }]}
+                            rules={[
+                                { required: true, whitespace: true, message: '请输入密码' },
+                                {validator: this.validatePwd}
+                            ]}
                         >
                             <Input
                                 prefix={<LockOutlined className="site-form-item-icon" />}
@@ -43,24 +83,25 @@ export default class Login extends Component {
                             />
                         </Form.Item>
                         <Form.Item>
-                            <Form.Item name="remember" valuePropName="checked" >
-                                <Checkbox >Remember me</Checkbox>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox >记住账号</Checkbox>
                             </Form.Item>
-
                             <a className="login-form-forgot" href="">
-                                Forgot password
+                                忘记密码
                             </a>
                         </Form.Item>
 
                         <Form.Item >
-                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            <Button type="primary" htmlType="submit" className="login-form-button" >
                                 登录
                             </Button>
-                            Or <a href="">register now!</a>
+                            或者 <a href="">立即注册</a>
                         </Form.Item>
                     </Form>
                 </div>
+                <button onClick={this.aa}>+</button>
             </div>
+            
         )
     }
 }
